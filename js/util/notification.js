b00c84434e16d667bfa3ae0344ac65f251a34522
Tinysymphony <zjutiny@gmail.com>
2016-01-26 18:07:51 +0800

    fix

--- js/util/notification.js
+++ js/util/notification.js
@@ -3,16 +3,18 @@ var notifier = require('node-notifier');
 
 var Notice = function (title, body, link, img) {
     if (process.platform === 'win32') {
+        console.log('windows');
         notifier.notify({
             title: title,
             message: body,
             icon: img || 'file',
             sound: false
         });
-        notifier.on('click', function () {
-            window.open(link);
-        });
+        //notifier.on('click', function () {
+        //    window.open(link);
+        //});
     } else {
+        console.log('not windows');
         var notification = new Notification(title, {
             icon: img || 'file',
             body: body || 'notification contents'
@@ -27,7 +29,7 @@ var Notice = function (title, body, link, img) {
     // sound.volume = volume;
     // sound.play();
 
-    return notification;
+    //return notification;
 };
 
 module.exports = Notice;
